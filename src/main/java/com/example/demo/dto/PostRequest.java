53bfa5175bb89590dbba828806c6106fcbf3e3de
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2023-11-10 23:56:44 +0900

    DTO 생성자 제거

--- src/main/java/com/example/demo/dto/PostRequest.java
+++ src/main/java/com/example/demo/dto/PostRequest.java
@@ -1,16 +1,8 @@
 package com.example.demo.dto;
 
-import lombok.Getter;
-
 public class PostRequest {
-
 	private String title;
 	private String text;
-
-	public PostRequest(String title, String text) {
-		this.title = title;
-		this.text = text;
-	}
 
 	public String getTitle() {
 		return title;
--- src/main/java/com/example/demo/dto/PostUpdateRequest.java
+++ src/main/java/com/example/demo/dto/PostUpdateRequest.java
@@ -1,16 +1,9 @@
 package com.example.demo.dto;
 
 public class PostUpdateRequest {
-
 	private Long id;
 	private String title;
 	private String text;
-
-	public PostUpdateRequest(Long id, String title, String text) {
-		this.id = id;
-		this.title = title;
-		this.text = text;
-	}
 
 	public Long getId() {
 		return id;
